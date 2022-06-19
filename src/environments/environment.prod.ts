c73f56a3080ef277eb019c4ae2d11a6a8101450e
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-06-19 18:30:23 -0300

    Inicialização do projeto Soul Digital

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-fc3f1',
+    appId: '1:110214233004:web:609b7372ad729b92715809',
+    storageBucket: 'soul-digital-fc3f1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC2aG482nGztzuzsStG-O0r1yrW7rtwetw',
+    authDomain: 'soul-digital-fc3f1.firebaseapp.com',
+    messagingSenderId: '110214233004',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-fc3f1',
+    appId: '1:110214233004:web:609b7372ad729b92715809',
+    storageBucket: 'soul-digital-fc3f1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyC2aG482nGztzuzsStG-O0r1yrW7rtwetw',
+    authDomain: 'soul-digital-fc3f1.firebaseapp.com',
+    messagingSenderId: '110214233004',
+  },
   production: false
 };
 
